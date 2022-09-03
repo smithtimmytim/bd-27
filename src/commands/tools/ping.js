bbd620616a8db89eceb0dd8dad24cae1040fd5ab
Tim Smith <tim@smithtimmytim.com>
2022-09-02 17:57:13 -0700

    add ping command

---
+++ src/commands/tools/ping.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+// Command Config
+const config = {
+  name: 'ping',
+  description: 'Returns my ping!'
+}
+
+// Register Command
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName(config.name)
+    .setDescription(config.description),
+
+  async execute( interaction, discord ) {
+    const message = await interaction.deferReply({
+      fetchReply: true
+    });
+
+    const newMessage = `API Latency: ${discord.ws.ping}\nClient Ping: ${message.createdTimestamp - interaction.createdTimestamp}`;
+
+    await interaction.editReply({
+      content: newMessage
+    });
+  }
+
+}
